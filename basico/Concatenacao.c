f46a8acd18bd341d19688f6a4c84238495afda29
Marco Aurelio Queiroz <teoaa2@gmail.com>
2021-08-23 21:28:20 -0300

    programas basicos

---
+++ basico/Concatenacao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    int v1[] = {1, 2};
+    printf("%d\n", v1); // posição na memoria?
+
+    printf("v1[0] = %d\n", v1[0]);
+    printf("v1[1] = %d", v1[1]);
+
+    return 0;
+}
