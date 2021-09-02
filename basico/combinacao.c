3258f9aa262216470b06ff6e2acb5c54b10cc1cf
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-09-01 21:35:34 -0300

    melhorando o exercicio

--- basico/combinacao.c
+++ basico/combinacao.c
@@ -13,7 +13,14 @@ int combinacao(int numElementos, int numCasos) {
 }
 
 int main() {
-    int num1 = 3, num2= 2;
+    int num1 = 0, num2 = 0;
+
+    printf("Num1: ");
+    scanf("%d", &num1);
+
+    printf("Num2: ");
+    scanf("%d", &num2);
+	
     printf("Resultado da combinação C(%d, %d): %d", num1, num2, combinacao(num1, num2));
 
     return 0;
